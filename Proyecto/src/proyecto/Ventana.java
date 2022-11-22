215f218ef852fdb16dcd4103738e692fdb8058c4
Fer-1111 <fcorona2020@udec.cl>
2022-11-22 11:24:23 -0300

    cree clase Ventana y la clase JPanel, estan vacias por el momento

---
+++ Proyecto/src/proyecto/Ventana.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame{
+
+    public Ventana() {
+        setSize(1000, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
